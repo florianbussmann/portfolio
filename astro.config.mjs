77e24ef89e431e3e4adca7dc484419e161ea3c39
Florian Bussmann <hallo@florian-bussmann.de>
2025-08-03 13:08:53 +0000

    chore: update base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+const isDev = process.env.NODE_ENV === 'development';
+
 // https://astro.build/config
 export default defineConfig({
+  base: isDev ? '/' : '/portfolio/',
   vite: {
     plugins: [tailwindcss()]
   }
